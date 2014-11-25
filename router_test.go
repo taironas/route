c93061a4cc470990166bce428d09ed32bf416f5e
santiaago <santiago.ariassar@gmail.com>
2014-11-25 21:46:22 +0000

    add formating to router test :)

--- router_test.go
+++ router_test.go
@@ -2,55 +2,55 @@ package route
 
 import (
 	"fmt"
-  "net/http"
+	"net/http"
 	"net/http/httptest"
-  "testing"
+	"testing"
 )
 
 var endpoints = []string{"/test/handler/1", "/test/handler/2"}
 
 func TestRouter(t *testing.T) {
-  r := new(Router)
+	r := new(Router)
 	r.HandleFunc(endpoints[0], testHandler1)
-  r.HandleFunc(endpoints[1], testHandler2)
+	r.HandleFunc(endpoints[1], testHandler2)
 
-  server := httptest.NewServer(r)
-  defer server.Close()
+	server := httptest.NewServer(r)
+	defer server.Close()
 
-  for _, endpoint := range endpoints {
-    res, err := http.Get(server.URL+endpoint)
-    if err != nil {
-      t.Fatal(err)
-    }
+	for _, endpoint := range endpoints {
+		res, err := http.Get(server.URL + endpoint)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-  	if res.StatusCode != http.StatusOK {
-  		t.Fatal(err)
-  	}
-  }
+		if res.StatusCode != http.StatusOK {
+			t.Fatal(err)
+		}
+	}
 }
 
 func TestNotFoundRoute(t *testing.T) {
-  r := new(Router)
-  r.HandleFunc(endpoints[0], testHandler1)
-  r.HandleFunc(endpoints[1], testHandler2)
+	r := new(Router)
+	r.HandleFunc(endpoints[0], testHandler1)
+	r.HandleFunc(endpoints[1], testHandler2)
 
-  server := httptest.NewServer(r)
-  defer server.Close()
+	server := httptest.NewServer(r)
+	defer server.Close()
 
-  res, err := http.Get(server.URL+"/test/handler/3")
-    if err != nil {
-      t.Fatal(err)
-    }
+	res, err := http.Get(server.URL + "/test/handler/3")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  	if res.StatusCode != http.StatusNotFound {
-  		t.Fatal(err)
-  	}
+	if res.StatusCode != http.StatusNotFound {
+		t.Fatal(err)
+	}
 }
 
-func testHandler1(w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "testHandler1 has been reached!")
+func testHandler1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "testHandler1 has been reached!")
 }
 
-func testHandler2(w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "testHandler2 has been reached!")
+func testHandler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "testHandler2 has been reached!")
 }
