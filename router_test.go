59e7f73959b7bee2fd7e0b5b7b1b261d25ba6187
Remy Jourde <remy.jourde@gmail.com>
2014-11-26 12:56:26 +0100

    add test that has a /[0-9]/ handler
    Fixes #4

--- router_test.go
+++ router_test.go
@@ -24,7 +24,7 @@ func TestRouter(t *testing.T) {
 		}
 
 		if res.StatusCode != http.StatusOK {
-			t.Fatal(err)
+			t.Fatal(res)
 		}
 	}
 }
@@ -43,7 +43,41 @@ func TestNotFoundRoute(t *testing.T) {
 	}
 
 	if res.StatusCode != http.StatusNotFound {
+		t.Fatal(res)
+	}
+}
+
+func TestFoundRegexpRoute(t *testing.T) {
+	r := new(Router)
+	r.HandleFunc("/test/handler/[0-9]/hello", testHandlerHello)
+
+	server := httptest.NewServer(r)
+	defer server.Close()
+
+	res, err := http.Get(server.URL + "/test/handler/1/hello")
+	if err != nil {
 		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Fatal(res)
+	}
+}
+
+func TestNotFoundRegexpRoute(t *testing.T) {
+	r := new(Router)
+	r.HandleFunc("/test/handler/[0-9]/hello", testHandlerHello)
+
+	server := httptest.NewServer(r)
+	defer server.Close()
+
+	res, err := http.Get(server.URL + "/test/handler/a/hello")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusNotFound {
+		t.Fatal(res)
 	}
 }
 
@@ -53,4 +87,8 @@ func testHandler1(w http.ResponseWriter, r *http.Request) {
 
 func testHandler2(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "testHandler2 has been reached!")
+}
+
+func testHandlerHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "testHandlerHello has been reached!")
 }
